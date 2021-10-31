e8d38ee797f66a32ef6737a6be0cd5dc030f90fc
a155340 <93454683+a155340@users.noreply.github.com>
2021-10-31 17:51:41 -0500

    fix: config

--- restRoute.js
+++ restRoute.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res){
+    console.log('get-log', req.params, req.body);
     res.json({
         "sucess": true
     })
  });
 router.post('/', function(req, res){
+    console.log('post-log', req.params, req.body);
     res.json({
         "sucess": true
     })
