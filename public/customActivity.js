738c0558a7f886218f3fab5c65dcd6ccacf83c6e
a155340 <93454683+a155340@users.noreply.github.com>
2021-10-31 16:05:44 -0500

    fix: config

--- public/customActivity.js
+++ public/customActivity.js
@@ -2,8 +2,11 @@
 // We use this connection to talk to Journey Builder. You'll want to keep this
 // reference handy and pass it into your UI framework if you're using React, Angular, Vue, etc.
 const connection = new Postmonger.Session();
+let activity = null;
 
 $(function() {
+    // setup our ui event handlers
+    setupEventHandlers();
     // Journey Builder will trigger "initActivity" after it receives the "ready" event
     connection.on('initActivity', onInitActivity);
     connection.on('requestedInteractionDefaults', requestedInteractionDefaults);
@@ -23,6 +26,10 @@ $(function() {
 });
 
 function requestedInteractionDefaults(payload) {
+    // set the activity object from this payload. We'll refer to this object as we
+    // modify it before saving.
+    activity = payload;
+
     console.log('-------- requestedInteractionDefaults --------');
     console.log('payload\n', JSON.stringify(payload, null, 4));
     console.log('requestInteraction', payload);
@@ -45,3 +52,34 @@ function onInitActivity(payload) {
     console.log('activity:\n ', JSON.stringify(activity, null, 4));
     console.log('-------------------------------------------------');
 }
+
+function onDoneButtonClick() {
+    // we set must metaData.isConfigured in order to tell JB that
+    // this activity is ready for activation
+    activity['metaData'].isConfigured = true;
+
+    // you can set the name that appears below the activity with the name property
+    activity.name = 'Code Engagement';
+    // get the option that the user selected and save it to
+    console.log('------------ triggering:updateActivity({obj}) ----------------');
+    console.log('Sending message back to updateActivity');
+    console.log('saving\n', JSON.stringify(activity, null, 4));
+    console.log('--------------------------------------------------------------');
+
+    connection.trigger('updateActivity', activity);
+}
+
+function onCancelButtonClick() {
+    // tell Journey Builder that this activity has no changes.
+    // we wont be prompted to save changes when the inspector closes
+    connection.trigger('setActivityDirtyState', false);
+
+    // now request that Journey Builder closes the inspector/drawer
+    connection.trigger('requestInspectorClose');
+}
+
+function setupEventHandlers() {
+    $("#done").on('click', onDoneButtonClick);
+    $("#cancel").on('click', onCancelButtonClick);
+}
+
